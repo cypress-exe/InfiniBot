86982fd251890491db2b067d60d7be0a63df0e6c
cypress-exe <dbthayer26@gmail.com>
2024-11-10 00:42:06 -0700

    Updated run.bash to not give error message on non-failures.

--- run.bash
+++ run.bash
@@ -11,9 +11,3 @@ done
 
 # Run the Docker container with optional detached_string mode
 docker-compose -f ./.devcontainer/docker-compose.yml up ${detached_string}
-
-# Check if the run was successful
-if [ $? != 0 ]; then
-  echo "ERROR: Docker run failed."
-  exit 1
-fi
