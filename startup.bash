e4a62535d28e9f0caf313790c42c7dcb7e114b92
cypress-exe <dbthayer26@gmail.com>
2025-05-15 21:17:41 -0600

    Updated startup.bash
    Startup file now automatically pulls latest changes from git repo and
    builds a new image, all to ensure that the production launch code runs
    the latest version.

--- startup.bash
+++ startup.bash
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+# Used for production server startup
+
+# Pull latest changes from git repository
+git pull
+
 # Stop (Delete) previous container
 bash ./remove_container.bash
+
+# Build new image (no cache)
+bash ./build.bash
 
 # Run new container
 bash ./run.bash -d
